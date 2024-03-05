54ff3fbf0a8a287650fbd5c52a7bd432557846a2
Dani <danisweetwood@gmail.com>
2024-03-04 19:25:42 -0600

    added basic layout to header and footer, added css file

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+import 'bulma/css/bulma.min.css';
 
 function Footer() {
   return (
-    <footer>
+    <footer className='footer'>
+      <div className='content has-text-centered'>
       <p>I'm a footer</p>
+      </div>
     </footer>
   );
 }
