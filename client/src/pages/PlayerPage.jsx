f03f43666bbf6a9e76592528b5687a93d48dbfac
Dani <danisweetwood@gmail.com>
2024-03-05 20:32:08 -0600

    tweaked layout for player page to remove nested columns

--- client/src/pages/PlayerPage.jsx
+++ client/src/pages/PlayerPage.jsx
@@ -5,19 +5,15 @@ function PlayerPage() {
   return (
     <div className='content has-text-centered'>
       <h1 className='title'>Player Dashboard</h1>
+      <p className='subtitle is-6 pt-1'>Manage your characters and join campaigns.</p>
       <div className="columns">
         <Settings />
-        <div className="column">
-          <p className='subtitle'>Manage your characters and join campaigns.</p>
-          <div className='columns'>
-            <div className='column'>
-              <p className='subtitle'>Your Current Characters Here:</p>
-              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis minus voluptates illo et ipsa reprehenderit cumque dolore nesciunt delectus suscipit expedita aliquam, illum consectetur repudiandae laboriosam. Nulla cum tempora sit.</p>
-            </div>
-            <div className='column'>
-              <CharacterForm />
-            </div>
-          </div>
+        <div className='column'>
+          <p className='subtitle'>Your Current Characters Here:</p>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis minus voluptates illo et ipsa reprehenderit cumque dolore nesciunt delectus suscipit expedita aliquam, illum consectetur repudiandae laboriosam. Nulla cum tempora sit.</p>
+        </div>
+        <div className='column'>
+          <CharacterForm />
         </div>
       </div>
     </div>
