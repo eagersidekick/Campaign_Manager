2616a1a60579ea686d72092691c2da46794e29e0
Dani <danisweetwood@gmail.com>
2024-03-05 20:19:36 -0600

    added layout to character creation form / player page. added campaign page button in header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,6 +10,7 @@ function Header() {
         <Link className='navbar-item' to="/">Home</Link>
         <Link className='navbar-item' to="/player">Player Page</Link>
         <Link className='navbar-item' to="/tabletalk">Table Talk</Link>
+        <Link className='navbar-item' to="/campaign">Campaign</Link>
         </div>
         <div className='navbar-end'>
           <Link className='navbar-item' to='/login'>Login</Link>
