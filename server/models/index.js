b9fd4d94f76af694afe3dc45b96f48cc20208fe5
Ayden Krohn <krohnayden@gmail.com>
2024-03-05 18:40:46 -0600

    added index file to hold all models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Users = require('./Users');
+const Characters = require('./Character');
+const Campaign = require('./Campaign');
+const Inventory = require('./Inventory');
+
+module.exports = { Users, Characters, Campaign, Inventory};
