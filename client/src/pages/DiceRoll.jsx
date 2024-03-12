715b444f9debbd7bb18a7ff4d47eb987cfb23a4f
Dani <danisweetwood@gmail.com>
2024-03-12 16:25:11 -0500

    added functionality to dice roller

--- client/src/pages/DiceRoll.jsx
+++ client/src/pages/DiceRoll.jsx
@@ -1,33 +1,15 @@
-function DiceRoll() {
+import Roller from '../components/Roller';
 
-// function diceSelect(dice){
-
-// }
-
-function rollDice(max) {
-    return Math.floor(Math.random() * max + 1);
-};
-
-function refreshPage() {
-    window.location.reload()};
+export default function DiceRoll() {
 
     return (
       <div id="background-img" className='content has-text-centered silver-text'>
         <h1 id='welcome-text' className='silver-text title'>Roll Dice</h1>
         <div className='section'>
           <p className='subtitle silver-text'>Click to Roll Dice!</p>
-          <button class="button" onClick={refreshPage}>Reroll</button>
-            <p>D100 = {rollDice(100)}</p>
-            <p>D20 = {rollDice(20)}</p>
-            <p>D12 = {rollDice(12)}</p>
-            <p>D10 = {rollDice(10)}</p>
-            <p>D8 = {rollDice(8)}</p>
-            <p>D6 = {rollDice(6)}</p>
-            <p>D4 = {rollDice(4)}</p>
+            <Roller />
         </div>
       </div>
     );
   }
-  
-  export default DiceRoll;
   
